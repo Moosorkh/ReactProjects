c5d89a1cda52db180dac2ce9d1b8e9d8f144af68
Fortis Irdmousa <moosorkh@gmail.com>
2022-10-12 22:15:43 -0700

    Operation functionality added

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -21,10 +21,44 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+      case ACTIONS.CHOOSE_OPERATION:
+        if(state.currentOperand == null){
+          return state
+        }
+        if(state.previousOperand == null){
+          return {
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand: null,
+          }
+        }
+        return{
+          ...state,
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          customElements: null
+        }
   case ACTIONS.CLEAR:
     return {}
   }
 }
+
+function evaluate({currentOperand, previousOperand, operation}){
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if(isNaN(prev) || isNaN(current))return "";
+  let computation = "";
+  switch(operation){
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+  }
+}
+
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
 
