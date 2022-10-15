e01fe746396e4717fcdc3cd27352ddfe820c16ef
Fortis Irdmousa <moosorkh@gmail.com>
2022-10-15 06:57:09 -0700

    Evaluate is done

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -15,16 +15,32 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if(state.overwrite){
+        return{
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if(payload.digit === "0" && state.currentOperand === "0") return state;
       if (payload.digit === "." && state.currentOperand.includes(".")) return state;
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       };
+
       case ACTIONS.CHOOSE_OPERATION:
         if(state.currentOperand == null & state.previousOperand == null){
           return state
         }
+        //
+        if(state.currentOperand == null){
+          return{
+            ...state,
+            operation: payload.operation,
+          }
+        }
+
         if(state.previousOperand == null){
           return {
             ...state,
@@ -40,7 +56,19 @@ function reducer(state, { type, payload }) {
           customElements: null
         }
   case ACTIONS.CLEAR:
-    return {}
+    return {};
+    case ACTIONS.EVALUATE:
+      if(state.operation == null || state.currentOperand == null || state.previousOperand == null){
+        return state;
+      }
+      return{
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state)
+
+      }
   }
 }
 
@@ -95,7 +123,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button className="span-two"  onClick={()=> dispatch({ type: ACTIONS.EVALUATE})}>=</button>
     </div>
   );
 }
